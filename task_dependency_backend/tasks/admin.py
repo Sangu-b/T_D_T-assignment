997fc8ae51a8933064a522704171fa100119cbd4
sangamesh biradar <sangameshb510@gmail.com>
2026-01-21 15:09:42 +0530

    Register Task and TaskDependency models in Django admin

--- task_dependency_backend/tasks/admin.py
+++ task_dependency_backend/tasks/admin.py
@@ -1,3 +1,15 @@
-from django.contrib import admin
+# from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Task, TaskDependency
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'status', 'created_at')
+    list_filter = ('status',)
+    search_fields = ('title',)
+
+@admin.register(TaskDependency)
+class TaskDependencyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'task', 'depends_on', 'created_at')
